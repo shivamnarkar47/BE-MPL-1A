49afc98e17df23c857c6c42dadd99e5206d0c031
Shivam Narkar <shivamnarkar47@gmail.com>
2025-10-31 16:15:24 +0530

    Remove unused axios import and baseURL definition

--- repurpose-hub/src/lib/requestUrl.tsx
+++ repurpose-hub/src/lib/requestUrl.tsx
@@ -17,24 +17,4 @@ export const requestUrl = async <T = any>(data: DataProps): Promise<AxiosRespons
     responseType: data?.responseType || "json", // ✅ defaults to JSON
   })
 }
-import axios, { AxiosResponse } from "axios"
 
-const baseURL = "http://localhost:8000/"
-
-type DataProps = {
-  method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE"
-  endpoint: string
-  data?: {}
-  responseType?: "json" | "blob" | "text" | "arraybuffer" // ✅ optional
-}
-
-export const requestUrl = async <T = any>(data: DataProps): Promise<AxiosResponse<T>> => {
-  return axios({
-    method: data.method,
-    url: `${baseURL}${data.endpoint}/`,
-    data: data?.data,
-    responseType: data?.responseType || "json", // ✅ defaults to JSON
-  })
-}
-
-Add a feature where we can add type of response as optionl
